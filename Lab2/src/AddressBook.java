0ef9cbcc9a520d66f00611a7486530d1ad9d3f42
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-09-17 09:06:17 -0400

    I add some function calls in AddressBook.java to test Git commit

--- Lab2/src/AddressBook.java
+++ Lab2/src/AddressBook.java
@@ -19,5 +19,11 @@ public class AddressBook {
     public static void main (String []args){
         System.out.println("Address Book");
         System.out.println("Testing commit again");
+
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
