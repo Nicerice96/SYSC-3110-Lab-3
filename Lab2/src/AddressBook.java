fa86084737d89e8bbc6172f93c5462ace7cc3a28
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-09-17 08:57:07 -0400

    Updated main to print out "Address Book."

---
+++ Lab2/src/AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> Bud_list;
+    AddressBook(){
+        Bud_list = new ArrayList<BuddyInfo>();
+    }
+
+    private void addBuddy(BuddyInfo buddy){
+        Bud_list.add(buddy);
+    }
+
+    private void removeBuddy(BuddyInfo buddy){
+        Bud_list.remove(buddy);
+    }
+
+
+    public static void main (String []args){
+        System.out.println("Address Book");
+    }
+}
